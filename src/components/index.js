03080cb54fc990b07de6c15a22f339f28ac997f8
Mica-Shatil <71103380+Mica-Shatil@users.noreply.github.com>
2022-10-03 13:16:04 -0400

    Delete src directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Role } from './role/Role'
-export { default as Project } from './project/Project'
-export { default as Feature } from './feature/Feature'
-export { default as Navbar } from './navbar/Navbar'
